fa35a09459f9929214c17ad6416dac629c0c24a2
Nicolas Hery <hery.nicolas@gmail.com>
2015-11-07 10:53:46 -0500

    Complete Credit Card exercises

--- src/Homework/A/CreditCard.hs
+++ src/Homework/A/CreditCard.hs
@@ -3,10 +3,37 @@ module Homework.A.CreditCard where
 
 -- Exercise 1
 
-toDigits :: Integer -> [Integer]
-toDigits n
-  | n <= 0    = []
-  | otherwise = (toDigits (n `div` 10)) ++ [n `mod` 10]
-
+-- Convert positive Integers to a reversed list of digits
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = reverse . toDigits
+toDigitsRev n
+  | n <= 0    = []
+  | otherwise = (n `mod` 10) : (toDigitsRev (n `div` 10))
+
+-- Convert positive Integers to a list of digits
+toDigits :: Integer -> [Integer]
+toDigits = reverse . toDigitsRev
+
+-- Exercise 2
+
+-- Double every other Integer in a list
+doubleEveryOtherLeft :: [Integer] -> [Integer]
+doubleEveryOtherLeft []       = []
+doubleEveryOtherLeft (x:[])   = [x]
+doubleEveryOtherLeft (x:y:zs) = x : (y * 2) : doubleEveryOtherLeft zs
+
+-- Double every other Integer in a list, beginning from the right
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther = reverse . doubleEveryOtherLeft . reverse
+
+-- Exercise 3
+
+-- Sum all digits in a list with a mix of one-digit and two-digit numbers
+sumDigits :: [Integer] -> Integer
+sumDigits xs = sum (concatMap toDigits xs)
+
+-- Exercise 4
+
+-- Indicate whether an Integer could be a valid credit card number
+validate :: Integer -> Bool
+validate number = total `mod` 10 == 0
+  where total = (sumDigits . doubleEveryOther . toDigits) number
--- test/Homework/A/CreditCardSpec.hs
+++ test/Homework/A/CreditCardSpec.hs
@@ -7,6 +7,16 @@ import Test.Hspec
 spec :: Spec
 spec = describe "Homework.A.CreditCard" $ do
 
+  describe "toDigitsRev" $ do
+    it "converts positive Integers to a reversed list of digits" $ do
+      toDigitsRev 1234 `shouldBe` [4,3,2,1]
+
+    it "returns the empty list for 0 input" $ do
+      toDigitsRev 0 `shouldBe` []
+
+    it "returns the empty list for negative inputs" $ do
+      toDigitsRev (-17) `shouldBe` []
+
   describe "toDigits" $ do
     it "converts positive Integers to a list of digits" $ do
       toDigits 1234 `shouldBe` [1,2,3,4]
@@ -17,12 +27,20 @@ spec = describe "Homework.A.CreditCard" $ do
     it "returns the empty list for negative inputs" $ do
       toDigits (-17) `shouldBe` []
 
-  describe "toDigitsRev" $ do
-    it "converts positive Integers to a reversed list of digits" $ do
-      toDigitsRev 1234 `shouldBe` [4,3,2,1]
+  describe "doubleEveryOther" $ do
+    it "doubles every other Integer from the right for a list of even length" $ do
+      doubleEveryOther [8,7,6,5] `shouldBe` [16,7,12,5]
 
-    it "returns the empty list for 0 input" $ do
-      toDigitsRev 0 `shouldBe` []
+    it "doubles every other Integer from the right for a list of odd length" $ do
+      doubleEveryOther  [1,2,3] `shouldBe` [1,4,3]
 
-    it "returns the empty list for negative inputs" $ do
-      toDigitsRev (-17) `shouldBe` []
+  describe "sumDigits" $ do
+    it "sums all digits in a list with one or two-digit numbers" $ do
+      sumDigits [16,7,12,5] `shouldBe` 22
+
+  describe "validate" $ do
+    it "returns True for valid credit card numbers" $ do
+      validate 4012888888881881 `shouldBe` True
+
+    it "returns False for invalid credit card numbers" $ do
+      validate 4012888888881882 `shouldBe` False
