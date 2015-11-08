3fd6c2b013001ab48d8be88491854b5101e38c55
Nicolas Hery <hery.nicolas@gmail.com>
2015-11-08 14:09:13 -0500

    Small refactor of Hanoi exercise

--- src/Homework/A/Hanoi.hs
+++ src/Homework/A/Hanoi.hs
@@ -7,12 +7,11 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi n source target temp
- | n == 1 = [(source, target)]
- | otherwise =
-    -- First, move n-1 discs using "b" as the "temp" and "c" as the "target"
-    (hanoi (n - 1) source temp target) ++
-    -- Then, move the big disc from "a" ("source") to "b" ("target")
-    [(source, target)] ++
-    -- Finally, move n-1 discs using "c" as the "source" and "a" as the "temp"
-    (hanoi (n - 1) temp target source)
+hanoi 1 source target _ = [(source, target)]
+hanoi n source target temp =
+  -- First, move n-1 discs using "b" as the "temp" and "c" as the "target"
+  (hanoi (n - 1) source temp target) ++
+  -- Then, move the big disc from "a" ("source") to "b" ("target")
+  [(source, target)] ++
+  -- Finally, move n-1 discs using "c" as the "source" and "a" as the "temp"
+  (hanoi (n - 1) temp target source)
