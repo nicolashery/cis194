a957bf05d512fd37b301c5d86cffae9f033fd8a9
Nicolas Hery <hery.nicolas@gmail.com>
2015-11-11 20:27:02 -0500

    Add solution to histogram exercise

--- src/Homework/C/Golf.hs
+++ src/Homework/C/Golf.hs
@@ -16,3 +16,29 @@ localMaxima :: [Integer] -> [Integer]
 localMaxima xs@(x:y:z:_) =
   (if y > x && y > z then [y] else []) ++ (localMaxima (drop 1 xs))
 localMaxima _ = []
+
+-- Exercise 3 Histogram
+histogram :: [Integer] -> String
+histogram xs =
+  let buckets = [0,1,2,3,4,5,6,7,8,9]
+      -- List with the number of occurences for each integer (0-9)
+      frequences = map (\i -> length (filter ((==) i) xs)) buckets
+      -- List of tuples (integer, frequence) representing the histogram
+      pairs = zip buckets frequences
+      -- "Height" of the histogram is 2 lines (for the integers and "="s)
+      -- plus the maximum frequence
+      height = 2 + (foldl max 0 frequences)
+      -- Start by building a "flipped" version of the histogram, i.e.:
+      -- ["0=*  ", "1=**", etc.]
+      visualize (i, n) =
+        show i ++ "=" ++ replicate n '*' ++ replicate (height - n) ' '
+      flipped = map visualize pairs
+      -- "Expand" the flipped version to an intermediate representation:
+      -- [["0=*  ", "1=**"], ["=*  ", "=**"], ["*  ", "**"], etc.]
+      expanded = take height (iterate (map (drop 1)) flipped)
+      -- ..so we can take the first item of each sublist and create the lines:
+      -- ["01", "==", "**", etc.]
+      concatFirsts l = foldl (++) "" (map (take 1) l)
+      reversed = map concatFirsts expanded
+      -- Which we reverse and join with "\n"
+  in unlines (reverse reversed)
--- test/Homework/C/GolfSpec.hs
+++ test/Homework/C/GolfSpec.hs
@@ -4,6 +4,23 @@ module Homework.C.GolfSpec where
 import Homework.C.Golf
 import Test.Hspec
 
+histogram1 :: String
+histogram1 = unlines
+  [ " *        "
+  , " *        "
+  , " *   *    "
+  , "=========="
+  , "0123456789" ]
+
+histogram2 :: String
+histogram2 = unlines
+  [ "    *     "
+  , "    *     "
+  , "    * *   "
+  , " ******  *"
+  , "=========="
+  , "0123456789" ]
+
 spec :: Spec
 spec = describe "Homework.C.Golf" $ do
 
@@ -32,3 +49,10 @@ spec = describe "Homework.C.Golf" $ do
 
     it "returns multiple local maxima if they exist" $ do
       localMaxima [2,9,5,6,1] `shouldBe` [9,6]
+
+  describe "histogram" $ do
+    it "works for one input" $ do
+      histogram [1,1,1,5] `shouldBe` histogram1
+
+    it "works for another input" $ do
+      histogram [1,4,5,4,6,6,3,4,2,4,9] `shouldBe` histogram2
